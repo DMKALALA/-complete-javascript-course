a6fe44dc59445bc54218a3709672c6fddb241b61
DMKALALA <denisthe1st@gmail.com>
2023-05-26 22:32:52 -0400

    First submission

---
+++ complete-javascript-course-master (copy)/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+
+let country ='South Africa';
+let continent =' Africa';
+let population ='50 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ complete-javascript-course-master (copy)/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+
+/*let js = 'amazing';
+if (js === 'amazing') alert('Javascript is fun!');
+
+
+
+console.log(40 + 28 + 23 - 50);*/
+
+//Using Variables and Values:
+
+let js = 'amazing';
+console.log(40 + 28 + 23 - 50);
+//we use console.log() as a print operation to show our values in the console
+console.log("Denis");
+
+let firstName = "Denis";
+//to find out what is in the firstName variable we will output it using console.log()
+console.log(firstName)
+
+//let 5syntax = 5; this will create a syntax error
+
+// never use capital letters at the beginning of the variable as it is legal but only used in OOP when coding in  JS.
+
+//we use capitalized variable names for constant variables i.e let SCORE = 5;   the score will always be five
+let SCORE = 5;
